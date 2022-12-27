dc94e5200242bbc17396d48ca1854a79485e607f
Phuong <buiphuongaptutc@gmail.com>
2022-12-27 15:23:38 +0700

    feat: itests

---
+++ itests/src/main/java/io/github/censodev/itests/ItestsApplication.java
@@ -0,0 +1,13 @@
+package io.github.censodev.itests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItestsApplication.class, args);
+    }
+
+}
---
+++ itests/src/test/java/io/github/censodev/itests/ItestsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.censodev.itests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
